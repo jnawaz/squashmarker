ad19d6938561c3b67f673507bb9bc1ff8d264f9b
Jamil Nawaz <jamilnawaz88@gmail.com>
2023-01-19 20:30:40 +0000

    Navigation setup

---
+++ src/screens/landing/Landing.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Landing = () => {
+  return (
+    <>
+      <Text>Squash Marker</Text>
+    </>
+  );
+};
+
+export default Landing;
