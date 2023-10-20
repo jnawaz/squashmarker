0c5be93a6cac09397f08954090752ff63c83a6a5
Jamil Nawaz <jamilnawaz88@gmail.com>
2023-10-20 16:22:09 +0100

    AppTest fixed.

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -9,6 +9,12 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+jest.mock('@react-navigation/native');
+
+describe('App Tests', () => {
+  it('renders correctly', async () => {
+    await renderer.create(
+        <App />
+    );
+  });
 });
